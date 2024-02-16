9ac5bda7bf7131a439123e21a96eb2712fa9a7b9
Mukund Kumar <mukund.kr09@gmail.com>
2024-02-16 21:17:38 +0530

    first commit

---
+++ Components/NewsBoard.jsx
@@ -0,0 +1,8 @@
+
+export const NewsBoard = () => {
+  return (
+    <div>
+      <h2 className="text-center">Latest News <span className="">Today</span></h2>
+    </div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar';
+import { NewsBoard } from '../Components/NewsBoard';
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <NewsBoard/>
+    </div>
+  )
+}
+
+export default App;
